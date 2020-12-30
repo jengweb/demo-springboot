a8e5d3fcc89442bbcb7a0db3ad00e347e4ae18dc
Jengweb <jengweb@hotmail.com>
2020-12-30 15:00:15 +0700

    [Updated]: Unittest

--- src/main/java/com/example/rest/user/UserService.java
+++ src/main/java/com/example/rest/user/UserService.java
@@ -1,13 +1,15 @@
 package com.example.rest.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public UserModel inquiryUserById(int id) {
         try {
